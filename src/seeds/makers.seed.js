c25542926126563a5fa4d20a08c70dc2029c33f9
NamaWorks <pedronayamartinez@hotmail.com>
2024-04-04 21:32:52 +0200

    start creating makers seed

---
+++ src/seeds/makers.seed.js
@@ -0,0 +1,28 @@
+// Honda Kawasaki Yamaha Suzuki Ducati BMW Triumph KTM Aprilia MV Agusta Harley-Davidson Indian 
+
+const makers = [
+    {
+        makerName: "Honda",
+        country: "Japan",
+        foundationYear: 1949,
+        founder: "Sōichirō Honda",
+    },
+    {
+        makerName: "Kawasaki",
+        country: "Japan",
+        foundationYear: 1896,
+        founder: "Shozo Kawasaki",
+    },
+    {
+        makerName: "Yamaha",
+        country: "Japan",
+        foundationYear: 1887,
+        founder: "Torakusu Yamaha",
+    },
+    {
+        makerName: "Suzuki",
+        country: "Japan",
+        foundationYear: 1909,
+        founder: "Michio Suzuki",
+    },
+]
