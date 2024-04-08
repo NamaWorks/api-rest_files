9c976128580a366245df79a521e890bc2f6cbc52
NamaWorks <pedronayamartinez@hotmail.com>
2024-04-08 15:24:01 +0200

    create users feed data

--- src/seeds/users.seed.js
+++ src/seeds/users.seed.js
@@ -1,7 +1,50 @@
 const mongoose = require("mongoose")
 const User = require("../api/models/user_model")
 
-const users = []
+const users = [
+    {
+        userName: "Admin",
+        email: "admin@admin.com",
+        password: "Admin123!@",
+        profileImage: "#",
+        role: "admin",
+    },
+    {
+        userName: "Juan",
+        email: "juan@email.com",
+        password: "Juan123!@",
+        profileImage: "#",
+        role: "user",
+    },
+    {
+        userName: "Clara",
+        email: "clara@email.com",
+        password: "Clara123!@",
+        profileImage: "#",
+        role: "user",
+    },
+    {
+        userName: "Rosa",
+        email: "rosa@email.com",
+        password: "Rosa123!@",
+        profileImage: "#",
+        role: "user",
+    },
+    {
+        userName: "Pepe",
+        email: "pepe@email.com",
+        password: "Pepe123!@",
+        profileImage: "#",
+        role: "user",
+    },
+    {
+        userName: "Alex",
+        email: "alex@email.com",
+        password: "Alex123!@",
+        profileImage: "#",
+        role: "user",
+    },
+]
 
 const usersDocuments = users.map(user=>new User(user))
 
